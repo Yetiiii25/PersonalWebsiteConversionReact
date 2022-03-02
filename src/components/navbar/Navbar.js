c3b54cc35a2310138ee21a1260b28b4320570f93
Deinennn <81692661+Yetiiii25@users.noreply.github.com>
2022-03-02 14:39:24 +0800

    -

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,18 @@ import './NavbarStyle.css'
 
 
 function Navbar(){
-
+  // const doc = document;
+  // const menuOpen = doc.querySelector(".menu");
+  // const menuClose = doc.querySelector(".close");
+  // const overlay = doc.querySelector(".overlay");
+  
+  // menuOpen.addEventListener("click", () => {
+  //   overlay.classList.add("overlay--active");
+  // });
+  
+  // menuClose.addEventListener("click", () => {
+  //   overlay.classList.remove("overlay--active");
+  // });
  
     return (
       <>
